b7842a2098f9436fc8e2f46a51b2cef0603cceaa
ponCHANNN <mizuki.7302@gmail.com>
2026-02-11 20:14:46 +0900

    Add File Write

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -1,4 +1,4 @@
-module.exports = (client, o, n) => {
+module.exports = async (client, o, n) => {
   const me = client.users.cache.get("739006634533060702");
   if (!o) return;
   if (o.userId != "745279748053401601") return;
@@ -6,6 +6,7 @@ module.exports = (client, o, n) => {
   const nowD = new Date();
   if (o.status != n.status) {
     me.send(`${o.user.globalName}さんが${nowD.getHours()}時${nowD.getMinutes()}分${nowD.getSeconds()}秒に${o.status}から${n.clientStatus ? (n.clientStatus.desktop ? "デスクトップで" : n.clientStatus.mobile ? "モバイルで" : n.clientStatus.web ? "ウェブサイトで" : "") : ""}${n.status}になりました。`);
+    await updateStatusFile(n.status);
   } else if (JSON.stringify(o.clientStatus) != JSON.stringify(n.clientStatus)) {
     if (!o.clientStatus.desktop && o.clientStatus.mobile && n.clientStatus.desktop) {
       me.send(`${o.user.globalName}さんが${nowD.getHours()}時${nowD.getMinutes()}分${nowD.getSeconds()}秒にスマホからPC:${n.clientStatus.desktop}になりました。`);
@@ -20,3 +21,9 @@ module.exports = (client, o, n) => {
   else if (o.activities[0] && n.activities[0] && o.activities[0].name !== "Hang Status" && n.activities[0].name !== "Hang Status" && o.activities[0].name !== n.activities[0].name)
     me.send(`${o.user.globalName}さんが${nowD.getHours()}時${nowD.getMinutes()}分${nowD.getSeconds()}秒に${o.activities[0].name}から${n.activities[0].name}に変更しました。`);
 };
+
+async function updateStatusFile(statusText) {
+  const fs = require("fs");
+  const path = "/tmp/discord_status.txt";
+  fs.writeFileSync(path, statusText);
+}
